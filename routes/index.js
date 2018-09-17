04835f22cd4beaa79cb7180c7e5a4048bd9babdb
denparf <denparf@gmail.com>
2018-09-17 07:28:38 +0300

    models created

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const den = { name: "Denis", age: 28, happy: true};
-  // res.send('Hey! It works!');
-  // res.send(req.query.name);
-    res.render('hello', {dog: req.query.dog});
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
